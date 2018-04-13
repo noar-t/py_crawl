f06e74b3d0bcdfc8cd575753555d9d92996e4998
Noah Thornton <noahthornton15@gmail.com>
2018-04-12 21:47:19 -0500

    hanging at unknown spot

--- crawl.py
+++ crawl.py
@@ -6,24 +6,34 @@ import sys
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 
-workers = 10
+workers = 2
 writers = 1
-depth = 3
+depth = 2
 
 
 # simple skeleman to show how to get work from the queue
-def worker(q_in, q_out, s, lock):
+def worker(q_in, s, lock):
     print('worker')
     # Do work while queue has stuff
     while True:
-        depth, url = q_in.get()
+        depth, url = q_in.get(timeout=5)
+
+        if depth == 0:
+            continue
         if url is None:
             break
-        lock.acquire()
 
-        if url not in s:
+        print('lock')
+        lock.acquire()
+        stale_url = url in s
+        if not stale_url:
             s.add(url)
-            lock.release()
+        lock.release()
+        print('unlock')
+
+        if not stale_url:
+            s.add(url)
+            print(url)
             try:
                 page = requests.get(url)
                 if page.ok and 'text/html' in page.headers['content-type']:
@@ -31,27 +41,26 @@ def worker(q_in, q_out, s, lock):
                     page_urls = get_urls(soup, url)
                     [q_in.put((depth - 1, u)) for u in page_urls]
                     page_text = text_from_soup(soup)
-                    q_out.put((page_urls, page_text))
-                    print((page_urls, page_text))
                     # TODO save page_urls to file
                     # TODO save text_from_soup
 
 
-            except Exception:
+            except Exception as e:
                 print('DEBUG exception in request')
+                #print('DEBUG exception in request' + e)
         else:
-            print('DEBUG duplicate')
-            lock.release()
-
-        q_in.task_done()
+            print('DEBUG duplicate: ' + url)
 
 # Get urls in a page and return it as a list
 def get_urls(soup, parent_url):
     urls = list()
     for url in soup.find_all('a', href=True):
         url_str = urllib.parse.urljoin(parent_url, url['href'])
-        urls.append(url_str)
-        #print('adding ' + url_str)
+        u_tuple = urllib.parse.urlsplit(url_str)
+        url_str = u_tuple.scheme + '://' + u_tuple.netloc + u_tuple.path
+        if u_tuple.scheme == 'http' or u_tuple.scheme == 'https':
+            #print(url_str)
+            urls.append(url_str)
 
     return urls
 
@@ -73,8 +82,8 @@ def tag_visible(element):
 
 def main():
     # Initialize synchronization data structures
-    q_in = multiprocessing.JoinableQueue()
-    q_out = multiprocessing.JoinableQueue()
+    q_in = multiprocessing.Queue()
+    #q_out = multiprocessing.JoinableQueue()
     lock = multiprocessing.Lock()
 
     url = 'https://www.cs.utexas.edu/~ans/classes/cs439/schedule.html'
@@ -88,17 +97,14 @@ def main():
 
     processes = list()
     for i in range(workers):
-        p = multiprocessing.Process(target=worker, args=(q_in, q_out, s, lock,))
+        p = multiprocessing.Process(target=worker, args=(q_in, s, lock,))
         processes.append(p)
         processes[i].start()
 
-    #q.put(None)
-    q_in.join()
 
     # signal all processes to stop and join them
     for i in range(workers):
-        q_in.put(None)
-    for i in range(workers):
+        print('joined: ' + i)
         processes[i].join()
 
 if __name__ == '__main__':
